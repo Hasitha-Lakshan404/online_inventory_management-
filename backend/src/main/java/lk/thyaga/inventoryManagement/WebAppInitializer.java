3938aa03c0d8908d1a60939920d5e76a4c46ec25
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2023-07-06 00:05:09 +0530

    initializing project

---
+++ backend/src/main/java/lk/thyaga/inventoryManagement/WebAppInitializer.java
@@ -0,0 +1,20 @@
+package lk.thyaga.inventoryManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : Hasitha Lakshan
+ * Project :thyaga_task
+ * Date :7/5/2023
+ * Time :11:13 PM
+ */
+
+@SpringBootApplication
+public class WebAppInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppInitializer.class);
+    }
+
+
+}
